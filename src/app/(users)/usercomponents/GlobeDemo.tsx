2371504ccaec41423a8dde3f269d99dd4aa6b4ed
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-04-05 19:55:09 +0545

    push

--- src/app/(users)/usercomponents/GlobeDemo.tsx
+++ src/app/(users)/usercomponents/GlobeDemo.tsx
@@ -415,7 +415,7 @@ export function GlobeDemo() {
          
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-        <div className="absolute w-full -bottom-20 h-[100vh] md:h-full z-10">
+        <div className="absolute w-full -bottom-20 h-[100%] md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />;
         </div>
       </div>
