f9b955dc58f6f748a40cc9641ad901798ee87b0e
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-04-16 22:39:51 +0545

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import React, { useEffect } from 'react'
 import { CldImage } from 'next-cloudinary';
-import { HeroSlider } from './(users)/usercomponents/HeroSlider';
+
 import Home from './(users)/Home/page';
 import { useSession } from 'next-auth/react';
  
