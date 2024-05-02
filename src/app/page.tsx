c2e31bcb9fbefe6aabbfc9cc71bfa343c9cfe4dc
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-05-02 20:05:54 +0545

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,12 @@ function Home1() {
     <div>
       <div className=' flex flex-row justify-end gap-2 m-2'><Button className=' bg-blue-700 font-bold text-md' onClick={()=>{
         HandleUserLogin()
-      }}>User Login</Button> <Button className=' bg-blue-700 font-bold text-md' onClick={()=>{
+      }}>User Login</Button>
+       {/* <Button className=' bg-blue-700 font-bold text-md' onClick={()=>{
         router.push("/adminlogin")
-      }}>Admin Login</Button></div>
+      }}>Admin Login</Button> */}
+      
+      </div>
       <div className=' m-auto flex  flex-col-reverse flex-wrap justify-between items-center md:flex-row lg:flex-row'>
         <div className=' w-full md:w-[40%] lg:w-[45%] '><MainHomeHero /></div>
         <div className=' w-full md:w-[50%] lg:w-[45%]'><Lottie animationData={SearchImage}></Lottie></div>
